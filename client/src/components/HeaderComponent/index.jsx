62cfcee02c022d3f97dd431274aa3ec2fe692cfd
Aibier <aibiertony@gmail.com>
2019-12-10 00:08:55 +0800

    2019-12-09: make a sticky header

--- client/src/components/HeaderComponent/index.jsx
+++ client/src/components/HeaderComponent/index.jsx
@@ -8,7 +8,7 @@ class HeaderComponent extends React.Component {
         const { user } = this.props;
         let data;
         const divStyle = { width: '100px', paddingRight: '10px' };
-        data =  <div className="col-md-12">
+        data =  <div className="col-md-12 header_container">
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <Link to="/" className="navbar-brand" >Home</Link>
                 <div className="collapse navbar-collapse">
@@ -18,9 +18,9 @@ class HeaderComponent extends React.Component {
                 {user && user.user ?
                     (<div>
                         <a className="" style={divStyle}>Hi {user.user.name}!</a>
-                        <Link to="/login" className="btn btn-outline-success my-2 my-sm-0" >Logout</Link>
+                        <Link to="/login" className="btn btn-outline-success my-2 my-sm-0" >Singout</Link>
                     </div>) : (
-                        <Link to="/login" className="btn btn-outline-success my-2 my-sm-0" >Login</Link>
+                        <Link to="/login" className="btn btn-outline-success my-2 my-sm-0" >Signin</Link>
                     )
                 }
             </nav>
